95e17b62462fbac8561c7810b473d9975d826e79
Alberto Vena <kennyadsl@gmail.com>
2014-07-18 16:21:25 +0200

    removes sitemap resource
    it's much better to use a separate extension like:
    https://github.com/kennyadsl/spree-sitemap-generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :sitemap
-
-  #match '/product_list.:format' => "sitemap#product_list"
-
   namespace :admin do
     resource :keywords_footer
     resource :sitewide_seo
